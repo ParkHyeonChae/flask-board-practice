49933e47c3cb62e7498dea7e2f3820e4546287dc
Park Hyeon Chae <park19996@naver.com>
2019-08-05 03:00:03 +0900

    'commit'

---
+++ app.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template, request, session, url_for 
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    session['logged'] = False
+    return render_template('index.html')
+
+@app.route('/login')
+def login(): 
+    return render_template('login.html')
+
+@app.route('/regist')
+def regist(): 
+    return render_template('regist.html')        
+
+@app.route('/main', methods=['POST'])
+def main():
+    if request.method == 'POST':
+        if(request.form['id'] == '현채' and request.form['pw'] == '123'):
+            session['logged'] = True
+            session['user'] = request.form['id']
+            #return 'Hi, ' + request.form['id']
+            return render_template('main.html')
+        else:
+            return """<script>alert("wrong!");location.href='/login';</script>"""
+    else:
+        return """<script>alert("not allowd!");location.href='/login';</script>"""
+app.secret_key = 'sample_secret'
+
+if __name__ == '__main__':
+    app.run()
